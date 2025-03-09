797a02ceaebe013a9b9992ea09d4c6207d17c509
Pepijn <pepijndevue@hotmail.com>
2025-03-09 12:13:07 +0100

    Voor onze Yunus <3

---
+++ src/log.py
@@ -0,0 +1,11 @@
+def log_sim(ds):
+    # log list with evac times, avg evac time, total evac time
+
+    # read cnt 
+    cnt = 0
+
+    # calc log stuff
+    pass
+
+    # write log
+    # cnt.json = dslkjfldssf
